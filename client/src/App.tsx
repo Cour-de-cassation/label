8a67272e57471603d495ffcaa2faa642acda79c9
BenoitSerrano <benoit.serrano10@gmail.com>
2020-09-03 17:52:07 +0200

    Add linter and prettier to client (#7)
    * add linter to client
    
    * add prettier to the client
    
    * add lint command
    
    * fix eslint configuration
    
    * add lint command
    
    * run linter in the project
    
    * add format on save feature
    
    * run eslint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 
-const App = () => 
-  (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;

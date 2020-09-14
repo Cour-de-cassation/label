fd63be88274efb17f925911c44b8e4d301774d12
Nicolas ASSOUAD <nicolas@assouad.com>
2020-09-14 16:51:18 +0200

    35 script to convert xml court decision to plain text court decision (#39)
    * add a script to package.json to run script
    
    * new script to convert xml court decsion to plain text court decision

---
+++ packages/backend/src/scripts/convertCourtDecisionsFromXmlToPlainText.ts
@@ -0,0 +1,57 @@
+import { promises as fs, write } from 'fs';
+import { converter } from '../lib/converter';
+
+const courtDecisionFolderPath = './tmp/xmlCourtDecisions/';
+const plainTextCourtDecisionFolderPath = './tmp/plainTextCourtDecisions/';
+
+convertCourtDecisionsFromXmlToPlainText();
+
+async function convertCourtDecisionsFromXmlToPlainText() {
+  console.log('Begin the convertion');
+
+  const courtDecisionFileNames = await getCourtDecisionFileNames();
+  console.log(`Files to convert ${courtDecisionFileNames}`);
+
+  await fs.mkdir(plainTextCourtDecisionFolderPath);
+
+  for (const ind in courtDecisionFileNames) {
+    await convertCourtDecisionFromXmlToPlainText(courtDecisionFileNames[ind]);
+  }
+}
+
+async function getCourtDecisionFileNames() {
+  const directory = await fs.opendir(courtDecisionFolderPath);
+  const courtDecisionFileNames: Array<string> = [];
+
+  for await (const entry of directory) {
+    if (entry.isFile()) {
+      courtDecisionFileNames.push(entry.name);
+    }
+  }
+
+  return courtDecisionFileNames;
+}
+
+async function convertCourtDecisionFromXmlToPlainText(
+  courtDecisionFileName: string,
+) {
+  console.log(`Converting ${courtDecisionFolderPath}${courtDecisionFileName}`);
+  const xmlCourtDecision = await fs.readFile(
+    `${courtDecisionFolderPath}${courtDecisionFileName}`,
+    {
+      encoding: 'latin1',
+    },
+  );
+  const parsedCourtDecision = converter.convertFromXml(xmlCourtDecision);
+  const plainTextCourtDecision = parsedCourtDecision.text;
+  const newCourtDecisionFileName = `${courtDecisionFileName.slice(
+    0,
+    courtDecisionFileName.length - 3,
+  )}txt`;
+  await fs.writeFile(
+    `${plainTextCourtDecisionFolderPath}${newCourtDecisionFileName}`,
+    plainTextCourtDecision,
+    'latin1',
+  );
+  console.log(`Done with ${courtDecisionFolderPath}${courtDecisionFileName}`);
+}

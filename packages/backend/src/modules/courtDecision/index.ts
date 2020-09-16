f16550159a1b8dc2da96d2f9dd919c7e40ce4b9d
Nicolas Assouad <nicolas@assouad.com>
2020-09-16 17:57:01 +0200

    repair export of court decision module

--- packages/backend/src/modules/courtDecision/index.ts
+++ packages/backend/src/modules/courtDecision/index.ts
@@ -1,7 +1,21 @@
-export {
+import {
   buildCourtDecisionGraphQLType,
   courtDecisionsGraphQLEntity,
   courtDecisionsQuery,
   insertCourtDecisionQuery,
   successGraphQLType,
 } from './graphQl';
+import {
+  buildCourtDecisionRepository,
+  buildFakeCourtDecisionRepository,
+} from './repository';
+
+export {
+  buildCourtDecisionGraphQLType,
+  courtDecisionsGraphQLEntity,
+  courtDecisionsQuery,
+  insertCourtDecisionQuery,
+  successGraphQLType,
+  buildCourtDecisionRepository,
+  buildFakeCourtDecisionRepository,
+};

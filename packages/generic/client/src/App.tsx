dce3e1f63bd38efb573cb3d2f3b27560beb5bf3c
BenoitSerrano <benoit.serrano10@gmail.com>
2020-09-28 17:42:55 +0200

    Setup router client (#82)
    * add stream option to client commands
    
    * add react router dom
    
    * add router implementation
    
    * add compile and test commands for client

--- packages/generic/client/src/App.tsx
+++ packages/generic/client/src/App.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { Home } from './pages/Home';
 import { client } from './services/apollo/client';
-// import { Login } from './pages/Login';
+import { Login } from './pages/Login';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App = () => (
   <ApolloProvider client={client}>
-    <Home />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </ApolloProvider>
 );
 

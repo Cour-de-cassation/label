1e85d03d71b4db2719e0e72c98632fbe58256da2
BenoitSerrano <benoit.serrano10@gmail.com>
2020-11-05 15:12:00 +0100

    add header in drawer settings (#306)

--- packages/generic/client/src/pages/Home/DocumentAnnotator/DocumentAnnotatorHeader/SettingsDrawer.tsx
+++ packages/generic/client/src/pages/Home/DocumentAnnotator/DocumentAnnotatorHeader/SettingsDrawer.tsx
@@ -1,12 +1,32 @@
+import { Theme, useTheme } from '@material-ui/core';
 import React from 'react';
-import { Drawer } from '../../../../components';
+import { Drawer, IconButton, LayoutGrid, Text } from '../../../../components';
+import { wordings } from '../../../../wordings';
 
 export { SettingsDrawer };
 
 function SettingsDrawer(props: { close: () => void; isOpen: boolean }) {
+  const theme = useTheme();
+  const styles = buildStyles(theme);
   return (
     <Drawer onClose={props.close} isOpen={props.isOpen}>
-      truc
+      <LayoutGrid alignItems="center" justifyContent="space-between" container style={styles.drawer}>
+        <LayoutGrid item>
+          <Text variant="h1">{wordings.settings}</Text>
+        </LayoutGrid>
+        <LayoutGrid item>
+          <IconButton hint={wordings.cancel} onClick={props.close} iconName="close" />
+        </LayoutGrid>
+      </LayoutGrid>
     </Drawer>
   );
+
+  function buildStyles(theme: Theme) {
+    return {
+      drawer: {
+        width: 600,
+        padding: theme.spacing(5),
+      },
+    };
+  }
 }
--- packages/generic/client/src/styles/theme.ts
+++ packages/generic/client/src/styles/theme.ts
@@ -19,13 +19,15 @@ const palette = {
 const fontSizes = {
   small: 12,
   medium: 16,
+  large: 24,
 };
 
 const typography = {
   body1: { fontFamily: 'Luciole', fontSize: fontSizes.medium },
   button: { fontFamily: 'Luciole', fontSize: fontSizes.medium },
   body2: { fontFamily: 'Courier New', fontSize: fontSizes.medium, lineHeight: '19px' },
-  h1: { fontFamily: 'Luciole-Bold', fontSize: fontSizes.medium },
+  h1: { fontFamily: 'Luciole', fontSize: fontSizes.large },
+  h2: { fontFamily: 'Luciole-Bold', fontSize: fontSizes.medium },
   subtitle1: { fontFamily: 'Luciole', fontSize: fontSizes.small, textDecoration: 'underline' },
 };
 

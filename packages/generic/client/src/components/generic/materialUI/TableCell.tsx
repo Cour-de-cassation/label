2aef14ebab615b84d37086dc8fc522d8226724f8
BenoitSerrano <benoit.serrano10@gmail.com>
2021-01-06 14:10:23 +0100

    fix border style for table (#536)

--- packages/generic/client/src/components/generic/materialUI/TableCell.tsx
+++ packages/generic/client/src/components/generic/materialUI/TableCell.tsx
@@ -3,6 +3,6 @@ import { TableCell as MuiTableCell } from '@material-ui/core';
 
 export { TableCell };
 
-function TableCell(props: { children?: ReactNode }) {
-  return <MuiTableCell>{props.children}</MuiTableCell>;
+function TableCell(props: { children?: ReactNode; classes: { root: string } }) {
+  return <MuiTableCell classes={props.classes}>{props.children}</MuiTableCell>;
 }
--- packages/generic/client/src/pages/Admin/Treatments/TreatmentTable/TreatmentTable.tsx
+++ packages/generic/client/src/pages/Admin/Treatments/TreatmentTable/TreatmentTable.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
 import { treatmentType } from '@label/core';
 import { Table, TableBody, TableCell, TableFooter, TableHead, TableRow, Text } from '../../../../components';
 import { wordings } from '../../../../wordings';
+import { customThemeType, useCustomTheme } from '../../../../styles';
 import { timeOperator } from './utils';
 
 export { TreatmentTable };
 
 function TreatmentTable(props: { treatments: treatmentType[] }) {
+  const theme = useCustomTheme();
+  const borderTableCellClasses = getBorderTableCellClasses(theme);
   const durations = props.treatments.map(({ duration }) => duration);
-
   return (
     <Table isHeaderSticky>
       <TableHead>
         <TableRow>
-          <TableCell>
+          <TableCell classes={borderTableCellClasses.bottom}>
             <Text variant="h3">{wordings.treatmentsPage.table.columnTitles.number}</Text>
           </TableCell>
-          <TableCell>
+          <TableCell classes={borderTableCellClasses.bottom}>
             <Text variant="h3">{wordings.treatmentsPage.table.columnTitles.duration}</Text>
           </TableCell>
         </TableRow>
@@ -24,10 +27,10 @@ function TreatmentTable(props: { treatments: treatmentType[] }) {
       <TableBody>
         {props.treatments.map((treatment) => (
           <TableRow>
-            <TableCell>
+            <TableCell classes={borderTableCellClasses.none}>
               <Text variant="h3">{treatment._id}</Text>
             </TableCell>
-            <TableCell>
+            <TableCell classes={borderTableCellClasses.none}>
               <Text variant="h3">{timeOperator.convertDurationToReadableDuration(treatment.duration)}</Text>
             </TableCell>
           </TableRow>
@@ -35,13 +38,13 @@ function TreatmentTable(props: { treatments: treatmentType[] }) {
       </TableBody>
       <TableFooter isSticky>
         <TableRow>
-          <TableCell>
+          <TableCell classes={borderTableCellClasses.top}>
             <Text variant="h3" color="textSecondary">
               {wordings.treatmentsPage.table.footer.total}
             </Text>
             <Text variant="h3">{wordings.treatmentsPage.table.footer.average}</Text>
           </TableCell>
-          <TableCell>
+          <TableCell classes={borderTableCellClasses.top}>
             <Text variant="h3" color="textSecondary">
               {timeOperator.getReadableTotalDuration(durations)}
             </Text>
@@ -51,4 +54,24 @@ function TreatmentTable(props: { treatments: treatmentType[] }) {
       </TableFooter>
     </Table>
   );
+}
+
+function getBorderTableCellClasses(theme: customThemeType) {
+  const top = makeStyles({
+    root: {
+      borderTop: `1px solid ${theme.colors.line.level2}`,
+      borderBottom: 'none',
+    },
+  })();
+  const bottom = makeStyles({
+    root: {
+      borderBottom: `1px solid ${theme.colors.line.level2}`,
+    },
+  })();
+  const none = makeStyles({
+    root: {
+      border: `none`,
+    },
+  })();
+  return { top, bottom, none };
 }

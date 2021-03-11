1d77e25607e368ea09870b34b1128ce53758dd59
Nicolas ASSOUAD <nicolas@assouad.com>
2021-03-11 11:54:13 +0100

    typo (#794)

--- scripts/preProd/export.sh
+++ scripts/preProd/export.sh
@@ -2,5 +2,5 @@
 
 cd packages/courDeCassation
 echo "Export the treated documents to SDER database"
-RUN_MODE=PREPROD node dist/crons/exportTreatedDocument.js  -e environments/preProdEnvironment.json -s settings/settings.json
+RUN_MODE=PREPROD node dist/crons/exportTreatedDocuments.js  -e environments/preProdEnvironment.json -s settings/settings.json
 cd ../../
--- scripts/prod/export.sh
+++ scripts/prod/export.sh
@@ -2,5 +2,5 @@
 
 cd packages/courDeCassation
 echo "Export the treated documents to SDER database"
-RUN_MODE=PROD node dist/crons/exportTreatedDocument.js  -e environments/prodEnvironment.json -s settings/settings.json
+RUN_MODE=PROD node dist/crons/exportTreatedDocuments.js  -e environments/prodEnvironment.json -s settings/settings.json
 cd ../../

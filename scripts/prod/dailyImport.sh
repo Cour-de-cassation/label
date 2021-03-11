dfe894d5ba8e3e11df6176455411087b708132b2
Nicolas ASSOUAD <nicolas@assouad.com>
2021-03-11 11:28:50 +0100

    Prod work (#792)
    * move start prod
    
    * move start preProd
    
    * daily import script
    
    * update prod

---
+++ scripts/prod/dailyImport.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd packages/courDeCassation
+echo "Import the documents from SDER database"
+RUN_MODE=PROD node dist/crons/importAllDocumentsFromSder.js  -e environments/prodEnvironment.json -s settings/settings.json
+echo "Annotate all the documents with the NLP engine"
+RUN_MODE=PROD node dist/crons/annotateDocumentsWithoutAnnotationsWithNlp.js  -e environments/prodEnvironment.json -s settings/settings.json
+cd ../../

da84ffc0e71c51794c119a27d5f06b50e762c1ea
Nicolas ASSOUAD <nicolas@assouad.com>
2021-03-16 14:58:06 +0100

    max size (#820)

--- packages/courDeCassation/src/connector/fetcher/sderFetcher.ts
+++ packages/courDeCassation/src/connector/fetcher/sderFetcher.ts
@@ -3,10 +3,14 @@ import { sderMapper } from './mapper';
 
 export { sderFetcher };
 
+const MAX_DOCUMENT_SIZE = 500000;
+
 const sderFetcher = {
   async fetchAllDocumentsSince(days: number) {
     const courtDecisions = await sderApi.fetchCourtDecisions(days);
 
-    return courtDecisions.map(sderMapper.mapCourtDecisionToDocument);
+    return courtDecisions
+      .map(sderMapper.mapCourtDecisionToDocument)
+      .filter(document => document.text.length <= MAX_DOCUMENT_SIZE);
   },
 };

aea2ae91a36579ed912d450ede17387798901fbe
Nicolas ASSOUAD <nicolas@assouad.com>
2021-03-22 10:48:03 +0100

    Update script (#851)
    * update script for pre prod
    
    * update script forprod

---
+++ scripts/prod/update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Check out prod version"
+git checkout prod
+
+echo "Pull last version"
+git pull
+
+echo "Install packages"
+yarn
+
+echo "Bootstrap"
+yarn bootstrap
+
+echo "Compile"
+yarn build
+
+echo "Run E2E test"
+cd packages/courDeCassation
+yarn testE2E

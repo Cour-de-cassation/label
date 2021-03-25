0689055cc3e78af04e1b90e8429e7a9e561aef06
Nicolas ASSOUAD <nicolas@assouad.com>
2021-03-25 15:42:51 +0100

    correct (#881)

--- scripts/preProd/deploy.sh
+++ scripts/preProd/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Stop LABEL service"
-services LabelServer stop
+systemctl stop LabelServer
 
 echo "Free port"
 fuser -k 55430/tcp
 
 echo "Start LABEL service"
-services LabelServer start
+systemctl stop LabelServer

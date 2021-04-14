034d229a4d688d34c882a477e9fb0fc45ddbc8db
Nicolas ASSOUAD <nicolas@assouad.com>
2021-04-14 17:56:55 +0200

    add new line in logs (#962)

--- packages/generic/backend/src/utils/logger/prodLogger.ts
+++ packages/generic/backend/src/utils/logger/prodLogger.ts
@@ -9,13 +9,13 @@ const prodLogger: loggerType = {
     const prettyValue = prettyLogFormatter.formatLog(value);
     // eslint-disable-next-line no-console
     console.log(prettyValue);
-    fileSystem.appendToFile('./logs.txt', prettyValue);
+    fileSystem.appendToFile('./logs.txt', `${prettyValue}\n`);
   },
 
   error(errorText) {
     const prettyError = prettyLogFormatter.formatErrorLog(errorText);
     // eslint-disable-next-line no-console
     console.error(prettyError);
-    fileSystem.appendToFile('./errors.txt', prettyError);
+    fileSystem.appendToFile('./errors.txt', `${prettyError}\n`);
   },
 };

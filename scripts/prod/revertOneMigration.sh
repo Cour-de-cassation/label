602855253679a8c34b0035c975b81424fbb9f521
BenoitSerrano <benoit.serrano10@gmail.com>
2021-08-31 08:47:21 +0200

    fix typo revertOneMigration (#1236)

--- scripts/prod/revertOneMigration.sh
+++ scripts/prod/revertOneMigration.sh
@@ -3,5 +3,5 @@ set -e
 
 cd packages/courDeCassation
 echo "Revert the last run migration"
-./scripts/runPreProdScript.sh dist/scripts/revertOneMigration.js
+./scripts/runProdScript.sh dist/scripts/revertOneMigration.js
 cd ../../

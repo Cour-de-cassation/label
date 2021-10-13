88598716973f18ec4d199a7af5b03cddd7f0e774
BenoitSerrano <benoit.serrano10@gmail.com>
2021-10-13 19:40:59 +0200

    Fix decisions with no categories to annotate (#1338)
    * upgrade yarn package
    
    * add migrations that delete adresse categories

---
+++ packages/generic/backend/src/app/scripts/deployment/migrations/26_61671296b1af2b8f48aa134c.ts
@@ -0,0 +1,37 @@
+import {
+  buildDocumentRepository,
+  documentService,
+} from '../../../../modules/document';
+import { logger } from '../../../../utils';
+
+export { up, down };
+
+async function up() {
+  logger.log('Up: ');
+
+  const documentRepository = buildDocumentRepository();
+
+  const documents = await documentRepository.findAll();
+
+  await Promise.all(
+    documents.map(async (document) => {
+      if (document.decisionMetadata.categoriesToOmit.includes('adresse')) {
+        await documentService.updateDocumentStatus(document._id, 'loaded');
+        return documentRepository.updateOne(document._id, {
+          decisionMetadata: {
+            ...document.decisionMetadata,
+            categoriesToOmit: [
+              'personneMorale',
+              'numeroSiretSiren',
+              'professionnelMagistratGreffier',
+            ],
+          },
+        });
+      }
+    }),
+  );
+}
+
+async function down() {
+  logger.log('Down: ');
+}

7a260c0762f5da640513569e197bfed6d2add266
BenoitSerrano <benoit.serrano10@gmail.com>
2021-11-02 15:57:48 +0100

    add entity count in time for agents (#1365)

--- packages/generic/client/src/pages/Home/DocumentAnnotator/DocumentPanel/DocumentPanel.tsx
+++ packages/generic/client/src/pages/Home/DocumentAnnotator/DocumentPanel/DocumentPanel.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useEffect } from 'react';
 import { assignationType } from '@label/core';
 import { apiCaller } from '../../../../api';
 import { useAnnotatorStateHandler } from '../../../../services/annotatorState';
+import { useDocumentViewerModeHandler } from '../../../../services/documentViewerMode';
 import { useViewerScrollerHandler } from '../../../../services/viewerScroller';
 import { customThemeType, useCustomTheme } from '../../../../styles';
 import { splittedTextByLineType } from '../lib';
@@ -10,26 +11,36 @@ import { DocumentViewer } from './DocumentViewer';
 
 export { DocumentPanel };
 
-const SCROLL_CHECK_INTERVAL = 2 * 60 * 1000;
+const SCROLL_CHECK_INTERVAL = 60 * 1000;
 
 function DocumentPanel(props: { splittedTextByLine: splittedTextByLineType }): ReactElement {
   const theme = useCustomTheme();
   const viewerScrollerHandler = useViewerScrollerHandler();
   const annotatorStateHandler = useAnnotatorStateHandler();
   const { assignationId } = annotatorStateHandler.get();
-
+  const documentViewerModeHandler = useDocumentViewerModeHandler();
   const styles = buildStyles(theme);
 
   let saveCursorPositionInterval: NodeJS.Timeout | null = null;
 
   useEffect(() => {
     if (!!assignationId) {
+      if (!!saveCursorPositionInterval) {
+        clearInterval(saveCursorPositionInterval);
+        apiCaller.post<'updateTreatmentDuration'>('updateTreatmentDuration', {
+          assignationId,
+        });
+      }
       saveCursorPositionInterval = setInterval(buildUpdateTreatmentUpdateDate(assignationId), SCROLL_CHECK_INTERVAL);
+
       return () => {
         saveCursorPositionInterval && clearInterval(saveCursorPositionInterval);
       };
     }
-  }, []);
+  }, [
+    documentViewerModeHandler.documentViewerMode.kind === 'occurrence' &&
+      documentViewerModeHandler.documentViewerMode.entityId,
+  ]);
 
   return (
     <div style={styles.panel}>

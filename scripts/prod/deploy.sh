3ae1fbeaa3543af7a8ee16b84971a174e104e925
antoinefa <fabien.antoine@m4x.org>
2022-01-05 22:04:52 +0100

    patch - fix front internal port in Docker

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -8,5 +8,5 @@ echo "Stop LABEL service"
 ( docker stop label-backend label-client && docker container rm label-backend label-client ) || echo
 
 echo "Start LABEL service"
-docker run -d -p 55432:80 --restart always --name label-client opendatajustice/label-client:latest
+docker run -d -p 55432:55432 --restart always --name label-client opendatajustice/label-client:latest
 docker run -d -p 55430:55430 --restart always --name label-backend opendatajustice/label-backend:latest

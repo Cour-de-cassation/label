b30ec256356fb1d24c8a74df806b218913c66d4f
antoinefa <fabien.antoine@m4x.org>
2022-02-08 20:59:53 +0100

    clean courDeCassation package from useless preprod files since k8s

--- packages/courDeCassation/scripts/runPreProdScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SCRIPT_NAME="$1"
-shift
-
-echo RUN_MODE=PREPROD node $SCRIPT_NAME -e environments/preProdEnvironment.json -s settings/settings.json $@
-RUN_MODE=PREPROD node $SCRIPT_NAME -e environments/preProdEnvironment.json -s settings/settings.json $@

09682eceb2477b26375bb438f1f36cbf46fd9517
David Libeau <DavidLibeau@users.noreply.github.com>
2022-03-04 15:50:14 +0100

    Fix displayMultipleAssignatedDocuments script (#1540)
    * Fix displayMultipleAssignatedDocuments script
    
    * Fix

--- packages/generic/backend/src/app/scripts/displayMultipleAssignatedDocuments.ts
+++ packages/generic/backend/src/app/scripts/displayMultipleAssignatedDocuments.ts
@@ -1,5 +1,8 @@
+import { idModule } from 'sder';
 import { logger } from '../../utils';
+import { assignationService } from '../../modules/assignation';
 import { buildDocumentRepository } from '../../modules/document';
+import { userService } from '../../modules/user';
 
 export { displayMultipleAssignatedDocuments };
 
@@ -10,13 +13,25 @@ async function displayMultipleAssignatedDocuments() {
 
   let documentCount = 0;
   for (const document of documents) {
-    const { viewerNames } = document.reviewStatus;
-    if (viewerNames.length > 1) {
+    const assignations = await assignationService.fetchAssignationsOfDocumentId(
+      document._id,
+    );
+    const usersByAssignationId = await userService.fetchUsersByAssignations(
+      assignations,
+    );
+    const userNames = assignations
+      ? assignations.map(
+          (assignation) =>
+            usersByAssignationId[idModule.lib.convertToString(assignation._id)]
+              .name,
+        )
+      : [];
+    if (userNames.length > 1) {
       documentCount++;
       logger.log(
         `${document.documentNumber} (${document.source}) has ${
-          viewerNames.length
-        } reviewers: [${viewerNames.join(', ')}]`,
+          userNames.length
+        } assignated: [${userNames.join(', ')}]`,
       );
     }
   }

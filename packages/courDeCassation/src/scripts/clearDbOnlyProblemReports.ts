c49f1bd5ddb413c7fcd167c3820d8da7591e3163
David Libeau <DavidLibeau@users.noreply.github.com>
2022-03-07 16:15:44 +0100

    Add clearDbOnlyProblemReports script (#1543)

---
+++ packages/courDeCassation/src/scripts/clearDbOnlyProblemReports.ts
@@ -0,0 +1,22 @@
+import { buildBackend } from '@label/backend';
+import { parametersHandler } from '../lib/parametersHandler';
+
+(async () => {
+  const { environment, settings } = await parametersHandler.getParameters();
+  const backend = buildBackend(environment, settings);
+
+  await backend.runScript(
+    () =>
+      backend.scripts.clearDb.run({
+        annotation: false,
+        assignation: false,
+        document: false,
+        migration: false,
+        monitoringEntry: false,
+        statistic: false,
+        treatment: false,
+        user: false,
+      }),
+    backend.scripts.clearDb.option,
+  );
+})();

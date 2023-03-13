7df055d9b80abfb1e35215f072419f5004e3b6b3
David Libeau <DavidLibeau@users.noreply.github.com>
2023-03-13 15:44:06 +0100

    Fix additionnal annotations (#26)

---
+++ packages/generic/core/src/modules/annotation/lib/removeOverlappingAnnotations.spec.ts
@@ -0,0 +1,28 @@
+import { annotationGenerator } from '../generator';
+import { removeOverlappingAnnotations } from './removeOverlappingAnnotations';
+
+describe('removeOverlappingAnnotations', () => {
+  it('should return annotations without overlaping ones', () => {
+    const annotations = [
+      { text: 'TEST', start: 1 },
+      { text: 'TRUC MACHIN', start: 10 },
+      { text: 'MACHIN', start: 15 },
+    ].map(annotationGenerator.generate);
+
+    const result = removeOverlappingAnnotations(annotations);
+
+    expect(result).toEqual([annotations[0], annotations[1]]);
+  });
+
+  it('should return all annotations because there is no overlapping', () => {
+    const annotations = [
+      { text: 'TEST', start: 1 },
+      { text: 'TRUC', start: 10 },
+      { text: 'MACHIN', start: 15 },
+    ].map(annotationGenerator.generate);
+
+    const result = removeOverlappingAnnotations(annotations);
+
+    expect(result).toEqual(annotations);
+  });
+});

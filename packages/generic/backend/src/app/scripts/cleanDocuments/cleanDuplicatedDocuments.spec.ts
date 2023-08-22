3b7aa9da659155e4cc88b1e2e934d0ba38894d0c
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2023-08-22 15:16:59 +0200

    Updating test to assess priority in duplicates deletion

--- packages/generic/backend/src/app/scripts/cleanDocuments/cleanDuplicatedDocuments.spec.ts
+++ packages/generic/backend/src/app/scripts/cleanDocuments/cleanDuplicatedDocuments.spec.ts
@@ -4,23 +4,26 @@ import { cleanDuplicatedDocuments } from './cleanDuplicatedDocuments';
 
 describe('cleanDuplicatedDocuments', () => {
   it('should clean the DuplicatedDocuments', async () => {
-    const originalDocument = documentModule.generator.generate();
-    const otherDocument = documentModule.generator.generate();
-    const doubledDocument = documentModule.generator.generate({
-      ...originalDocument,
+    const firstDocument = documentModule.generator.generate();
+    const secondDocument = documentModule.generator.generate();
+    const secondDocumentWithHigherStatus = documentModule.generator.generate({
+      ...secondDocument,
       _id: idModule.lib.buildId(),
+      status: 'done',
     });
     const documentRepository = buildDocumentRepository();
     await documentRepository.insertMany([
-      originalDocument,
-      otherDocument,
-      doubledDocument,
+      firstDocument,
+      secondDocument,
+      secondDocumentWithHigherStatus,
     ]);
 
     await cleanDuplicatedDocuments();
 
     const fetchedDocuments = await documentRepository.findAll();
 
-    expect(fetchedDocuments.sort()).toEqual([originalDocument, otherDocument]);
+    const fetchedIds = fetchedDocuments.map((u) => u._id).sort()
+    const expectedIds = [firstDocument, secondDocumentWithHigherStatus].map((u) => u._id).sort()
+    expect(fetchedIds).toEqual(expectedIds);
   });
 });

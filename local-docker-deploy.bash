a819214745e50e3ebe52360c7b2e37e2351849e8
Axel Marbois <axel.marbois@octo.com>
2023-10-13 16:53:44 +0200

    feat: partially add env variables for label deploy

--- local-docker-deploy.bash
+++ local-docker-deploy.bash
@@ -1,14 +1,18 @@
 #!/bin/bash
 # To deploy locally docker containers as in prod environment
 
-yarn install --
+#yarn install --
 
-yarn compile
+#yarn compile
 
-yarn buildClient
+#yarn buildClient
 
 docker build --target label-client -f Dockerfile.label-client -t label-client .
 
-docker run -it -p 127.0.0.1:8081:55432 --env-file=./.env-sample-label-client label-client
+docker build --target label-backend -f Dockerfile.label-backend -t label-backend .
 
+docker run -d -it -p 127.0.0.1:8081:55432 --env-file=./.env-sample-label-client label-client
 
+docker run -d -it -p 127.0.0.1:8082:55432 --env-file=./.env-sample-label-backend label-backend
+
+# erreur de serveur mongo, il faut lui donner une instance de mongo avec une base labelDb

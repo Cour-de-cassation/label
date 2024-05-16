f6a18ea60a2a48adbb33a83f223fdea97a80ebb2
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2024-05-16 15:12:04 +0200

    remove call to json env files

--- scripts/initializeTestDb.sh
+++ scripts/initializeTestDb.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "Reset the DB except for users and migrations"
-RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/clearDb.js -e packages/courDeCassation/environments/localEnvironment.json -s packages/courDeCassation/settings/settings.json
+RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/clearDb.js -s packages/courDeCassation/settings/settings.json
 echo "Import the documents from SDER database"
-RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/importAllDocumentsFromSderSinceOrBetween.js --fromDaysAgo 2  -e packages/courDeCassation/environments/localEnvironment.json -s packages/courDeCassation/settings/settings.json
+RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/importAllDocumentsFromSderSinceOrBetween.js --fromDaysAgo 2 -s packages/courDeCassation/settings/settings.json
 echo "Annotate all the documents with the NLP engine"
-RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/annotateDocumentsWithoutAnnotationsWithNlp.js  -e packages/courDeCassation/environments/localEnvironment.json -s packages/courDeCassation/settings/settings.json
+RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/annotateDocumentsWithoutAnnotationsWithNlp.js -s packages/courDeCassation/settings/settings.json
 echo "Insert mock values in DB"
-RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/initializeTestDb.js -e packages/courDeCassation/environments/localEnvironment.json -s packages/courDeCassation/settings/settings.json
+RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/initializeTestDb.js -s packages/courDeCassation/settings/settings.json
 echo "Create initial cache"
-RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/renewCache.js --beforeMinutes=5 -e packages/courDeCassation/environments/localEnvironment.json -s packages/courDeCassation/settings/settings.json
+RUN_MODE=LOCAL node packages/courDeCassation/dist/scripts/renewCache.js --beforeMinutes=5 -s packages/courDeCassation/settings/settings.json
 

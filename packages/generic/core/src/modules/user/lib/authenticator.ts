cac4330bb0a8cf1be6d3b80f6c28af168b466b53
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2024-05-31 16:49:48 +0200

    update vaults and env var

--- packages/generic/core/src/modules/user/lib/authenticator.ts
+++ packages/generic/core/src/modules/user/lib/authenticator.ts
@@ -1,5 +1,10 @@
 import { buildAuthenticator } from 'sder-core';
+import * as dotenv from 'dotenv';
 
 export { authenticator };
+
+if (process.env.RUN_MODE === 'LOCAL') {
+  dotenv.config();
+}
 
 const authenticator = buildAuthenticator(process.env.JWT_PRIVATE_KEY ?? 'local_private_key');

e0dce57014b51dba75b453bcf69b25b8963d7b95
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2024-07-31 15:45:58 +0200

    first tests and documentation of what I have try/undertand

--- packages/generic/client/src/pages/Home/DocumentAnnotator/lib/computeMultilineSelection.ts
+++ packages/generic/client/src/pages/Home/DocumentAnnotator/lib/computeMultilineSelection.ts
@@ -17,48 +17,54 @@ function computeMultilineSelection(
   anchorNodeValue: string,
   focusNodeValue: string,
 ) {
+  //la détextion de isMultilineSelection ne marche pas quand il y a plusieurs paragraphes
   const compiledText = [
     ...neighbours.before.reverse().map((textChunk) => textChunk.text),
     neighbours.current.text,
     ...neighbours.after.map((textChunk) => textChunk.text),
   ].join('');
+  // console.log(`compiledText : ${compiledText}`);
   const isMultilineSelection = compiledText.includes(anchorNodeValue) && compiledText.includes(focusNodeValue);
-  if (isMultilineSelection) {
-    const cleanedText = selectionText
-      .split('\t')
-      .map((lineText) => {
-        const match = lineText.replace(/\n[0-9]+\n/g, '').replace(/\n/g, '');
-        return match.trim();
-      })
-      .filter(Boolean);
-    if (neighbours.current.text.indexOf(cleanedText[0]) > -1) {
-      const nextTexts = neighbours.after.filter(({ text }) => !!text.trim());
-      const textSelection = cleanedText.reduce((accumulator, textLine) => {
-        const currentTextChunk = [neighbours.current, ...nextTexts].find(({ text }) => text.indexOf(textLine) > -1);
-        if (!currentTextChunk) {
-          return accumulator;
+  // console.log(`isMultilineSelection : ${isMultilineSelection}`);
+
+  if (true) {
+    console.log(`selectionText : ${JSON.stringify(selectionText)}`);
+    let cleannedText = selectionText.replace(/\n\d+\n/g, '\n'); //si il y a un nombre isolé sur une ligne ça n'entraine pas du bazar ?
+    console.log(`cleannedText : ${JSON.stringify(cleannedText)}`);
+
+    const splittedText = selectionText.split('\n');
+    const firstChunkText = splittedText[0];
+    const lastChunkText = splittedText[splittedText.length - 1];
+    console.log(`firstChunkText : ${JSON.stringify(firstChunkText)}`);
+    console.log(`lastChunkText : ${JSON.stringify(lastChunkText)}`);
+
+    // First chunk index can be found in current or before neighbour in case of selection from top to bottom or bottom to top
+    let firstChunkIndex = -1;
+
+    if (neighbours.current.text.indexOf(firstChunkText) > -1) {
+      firstChunkIndex = neighbours.current.index;
+      console.log('bottom to top');
+      cleannedText =
+        focusNodeValue + cleannedText.slice(firstChunkText.length, -lastChunkText.length) + anchorNodeValue;
+    } else {
+      for (let i = neighbours.before.length - 1; i >= 0; i--) {
+        const beforeNeighbour = neighbours.before[i];
+        if (beforeNeighbour.text.indexOf(firstChunkText) > -1) {
+          firstChunkIndex = beforeNeighbour.index;
+          break;
         }
-        return [
-          ...accumulator,
-          { text: textLine, index: currentTextChunk.index + currentTextChunk.text.indexOf(textLine) },
-        ];
-      }, [] as textSelectionType);
-      return textSelection;
-    } else if (neighbours.current.text.indexOf(cleanedText[cleanedText.length - 1]) > -1) {
-      const previousTexts = neighbours.before.filter(({ text }) => !!text.trim());
-      const textSelection = cleanedText.reduce((accumulator, textLine) => {
-        const currentTextChunk = [...previousTexts, neighbours.current].find(({ text }) => text.indexOf(textLine) > -1);
-        if (!currentTextChunk) {
-          return accumulator;
-        }
-        return [
-          ...accumulator,
-          { text: textLine, index: currentTextChunk.index + currentTextChunk.text.indexOf(textLine) },
-        ];
-      }, [] as textSelectionType);
-      return textSelection;
+      }
+
+      console.log('top to bottom');
+      cleannedText =
+        anchorNodeValue + cleannedText.slice(firstChunkText.length, -lastChunkText.length) + focusNodeValue;
     }
-    return [];
+
+    console.log(`final multiline : ${JSON.stringify({ text: cleannedText, index: firstChunkIndex })}`);
+
+    if (firstChunkIndex > -1) {
+      return [{ text: cleannedText, index: firstChunkIndex }];
+    }
   }
   return [];
 }

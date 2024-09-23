916b3fa1468cb90eab680dbbadf75e42ce78c10d
elhadj <elhadj.ndiaye@open-groupe.com>
2024-09-23 20:39:26 +0200

    Label : convert current session userId (_id: string) to mongo ObjectId

--- packages/generic/backend/src/api/buildAuthenticatedController/buildAuthenticatedController.ts
+++ packages/generic/backend/src/api/buildAuthenticatedController/buildAuthenticatedController.ts
@@ -1,13 +1,7 @@
-import { userModule, userType } from '@label/core';
+import { idModule, userModule, userType } from '@label/core';
 import { errorHandlers } from 'sder-core';
 
 export { buildAuthenticatedController };
-
-export type { authorizationHeaderType };
-
-type authorizationHeaderType = {
-  authorization: string;
-};
 
 function buildAuthenticatedController<inT, outT>({
   permissions,
@@ -21,15 +15,21 @@ function buildAuthenticatedController<inT, outT>({
 }): (req: { args: inT; headers: any; session?: any }) => Promise<outT> {
   return async (req: { args: inT; headers: any; session?: any }) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
-    const user =
+    const currentUser =
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       req.session && req.session.user ? req.session.user : null;
-    if (!user) {
+    if (!currentUser) {
       throw errorHandlers.authenticationErrorHandler.build(
         `user session has expired or invalid`,
       );
     }
-    userModule.lib.assertPermissions(user, permissions);
-    return controllerWithUser(user, req);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const dbUser = {
+      ...currentUser,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      _id: idModule.lib.buildId(currentUser._id),
+    };
+    userModule.lib.assertPermissions(dbUser, permissions);
+    return controllerWithUser(dbUser, req);
   };
 }

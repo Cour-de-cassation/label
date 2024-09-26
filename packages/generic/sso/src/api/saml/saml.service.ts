fb5cade8dd5e3d2faf76b7feea57f8b1c636bfe0
elhadj <elhadj.ndiaye@open-groupe.com>
2024-09-27 00:04:31 +0200

    Label : add new tests and run backend tests (config)

--- packages/generic/sso/src/api/saml/saml.service.ts
+++ packages/generic/sso/src/api/saml/saml.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import * as samlify from 'samlify';
 import * as fs from 'fs';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import * as validator from '@authenio/samlify-node-xmllint';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/ban-ts-comment
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 samlify.setSchemaValidator(validator);
 
 @Injectable()
@@ -31,11 +26,11 @@ export class SamlService {
       authnRequestsSigned: !isKeycloakIdp,
       wantAssertionsSigned: !isKeycloakIdp,
       isAssertionEncrypted: false, // keycloak
-      ...(isKeycloakIdp
+      ...(isKeycloakIdp || process.env.RUN_MODE === 'TEST'
         ? {}
         : {
             signingCert: fs.readFileSync(String(process.env.SSO_CERTIFICAT_VM), 'utf8'),
-            privateKey: fs.readFileSync(String(process.env.SSO_SP_PRIVATE_KEY), 'utf8'),
+            privateKey: fs.readFileSync(String(process.env.SSO_SP_PRIVATE_KEY_VM), 'utf8'),
           }),
     } as any;
 
@@ -43,9 +38,13 @@ export class SamlService {
     // Initialiser l'Identity Provider (IdP)
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const idpProps = {
-      metadata: fs.readFileSync(
-        isKeycloakIdp ? String(process.env.SSO_IDP_METADATA) : String(process.env.SSO_IDP_METADATA_VM),
-      ),
+      ...(process.env.RUN_MODE === 'TEST'
+        ? {}
+        : {
+            metadata: fs.readFileSync(
+              isKeycloakIdp ? String(process.env.SSO_IDP_METADATA) : String(process.env.SSO_IDP_METADATA_VM),
+            ),
+          }),
       wantAuthnRequestsSigned: !isKeycloakIdp,
       singleSignOnService: [
         {

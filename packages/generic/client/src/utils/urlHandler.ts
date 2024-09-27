977e3f205f1fffe6963856030329e46b516231c4
Zied ELLOUZE <zied.ellouze@open-groupe.com>
2024-09-27 12:19:35 +0200

    fix: ajout api url label

--- packages/generic/client/src/utils/urlHandler.ts
+++ packages/generic/client/src/utils/urlHandler.ts
@@ -2,13 +2,7 @@ export { urlHandler };
 
 const urlHandler = {
   getApiUrl() {
-    const clientPort = parseInt(window.location.port);
-    const clientProtocol = window.location.protocol;
-    const clientHostname = window.location.hostname;
-
-    const serverPort = clientPort - 2;
-
-    return serverPort ? `${clientProtocol}//${clientHostname}:${serverPort}` : `${clientProtocol}//${clientHostname}`;
+    return `${process.env.LABEL_API_URL}`;
   },
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types

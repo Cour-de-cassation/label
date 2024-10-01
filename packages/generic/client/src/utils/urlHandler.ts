e9fda7007dcb207e7c95b59cff14ee0a68da76da
Saad-Bouh Diallo <saad-bouh.diallo@open-groupe.com>
2024-10-01 11:42:32 +0200

    fix: api url

--- packages/generic/client/src/utils/urlHandler.ts
+++ packages/generic/client/src/utils/urlHandler.ts
@@ -2,16 +2,16 @@ export { urlHandler };
 
 const urlHandler = {
   getApiUrl() {
-    return `${process.env.REACT_APP_BACKEND_API_URL}`;
+    return `${process.env.REACT_APP_BACKEND_API_URL}` || `https://dev-label-api.teamlog.intra`;
   },
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   getSsoLoginUrl() {
-    return `${process.env.REACT_APP_BACKEND_API_URL}/label/api/sso/login`;
+    return `${getApiUrl()}/label/api/sso/login`;
   },
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   getSsoLogoutUrl() {
-    return `${process.env.REACT_APP_BACKEND_API_URL}/label/api/sso/logout`;
+    return `${getApiUrl()}/label/api/sso/logout`;
   },
 };

8c5d85b29e27cfd80ca0f4153aab2fd49efb0770
elhadj <elhadj.ndiaye@open-groupe.com>
2024-10-02 14:12:57 +0200

    fix: Compare String user _id to mongo id (ObjectId) and Back API url

--- packages/generic/client/src/utils/urlHandler.ts
+++ packages/generic/client/src/utils/urlHandler.ts
@@ -2,16 +2,17 @@ export { urlHandler };
 
 const urlHandler = {
   getApiUrl() {
-    return `https://dev-label-api.teamlog.intra`;
+    return process.env.REACT_APP_BACKEND_API_URL
+        ? `${process.env.REACT_APP_BACKEND_API_URL}` : `https://dev-label-api.teamlog.intra`;
   },
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   getSsoLoginUrl() {
-    return `https://dev-label-api.teamlog.intra/label/api/sso/login`;
+    return `${this.getApiUrl()}/label/api/sso/login`;
   },
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   getSsoLogoutUrl() {
-    return `https://dev-label-api.teamlog.intra/label/api/sso/logout`;
+    return `${this.getApiUrl()}/label/api/sso/logout`;
   },
 };

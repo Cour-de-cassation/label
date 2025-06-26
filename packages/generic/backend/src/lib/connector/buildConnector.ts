deeb936cfdb21c28e1c3fe4e7e9a909c322cd3a9
YATERA Boubacar <boubacar.yatera@justice.fr>
2025-06-26 11:10:40 +0200

    error on import decision doesn't stop all decisions imported

--- packages/generic/backend/src/lib/connector/buildConnector.ts
+++ packages/generic/backend/src/lib/connector/buildConnector.ts
@@ -62,9 +62,8 @@ function buildConnector(connectorConfig: connectorConfigType) {
 
       logger.log({
         operationName: 'importSpecificDocument',
-        msg: `Court decision found. labelStatus: ${
-          courtDecision.labelStatus
-        }, ${!!courtDecision.pseudoText ? 'already' : 'never'} pseudonymised`,
+        msg: `Court decision found. labelStatus: ${courtDecision.labelStatus
+          }, ${!!courtDecision.pseudoText ? 'already' : 'never'} pseudonymised`,
       });
       const document = await connectorConfig.mapCourtDecisionToDocument(
         courtDecision,
@@ -105,17 +104,17 @@ function buildConnector(connectorConfig: connectorConfigType) {
             courtDecision.labelTreatments == undefined
               ? []
               : courtDecision.labelTreatments[
-                  courtDecision.labelTreatments.length - 1
-                ].annotations.map((annotation) => {
-                  return annotationModule.lib.buildAnnotation({
-                    category: annotation.category,
-                    start: annotation.start,
-                    text: annotation.text,
-                    score: annotation.score,
-                    entityId: annotation.entityId,
-                    source: annotation.source,
-                  });
+                courtDecision.labelTreatments.length - 1
+              ].annotations.map((annotation) => {
+                return annotationModule.lib.buildAnnotation({
+                  category: annotation.category,
+                  start: annotation.start,
+                  text: annotation.text,
+                  score: annotation.score,
+                  entityId: annotation.entityId,
+                  source: annotation.source,
                 });
+              });
 
           await treatmentService.createTreatment(
             {
@@ -213,14 +212,21 @@ function buildConnector(connectorConfig: connectorConfigType) {
         decision !== undefined;
         decision = await newDecisionForSource.next()
       ) {
-        const converted = await connectorConfig.mapCourtDecisionToDocument(
-          decision,
-          'recent',
-        );
-        await insertDocument(converted, settings);
-        await connectorConfig.updateDocumentLabelStatusToLoaded(
-          converted.externalId,
-        );
+        try {
+          const converted = await connectorConfig.mapCourtDecisionToDocument(
+            decision,
+            'recent',
+          );
+          await insertDocument(converted, settings);
+          await connectorConfig.updateDocumentLabelStatusToLoaded(
+            converted.externalId,
+          );
+        } catch (err) {
+          logger.error({
+            operationName: 'importNewDocuments',
+            msg: `${err}`,
+          })
+        }
       }
     }
   }

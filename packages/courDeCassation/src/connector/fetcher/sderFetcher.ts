22872ab6499cb988b1307bc3513ecb845b3a3213
juliengrach-cassation <julien.grach@justice.fr>
2025-06-27 11:45:52 +0200

    Feat decisions paginated (#187)
    * add pagination from dbsder-api
    
    * label & juritcom harmony
    
    * fix lint
    
    * fix types
    
    * error on import decision doesn't stop all decisions imported
    
    * fixing lint
    
    ---------
    
    Co-authored-by: YATERA Boubacar <boubacar.yatera@justice.fr>

--- packages/courDeCassation/src/connector/fetcher/sderFetcher.ts
+++ packages/courDeCassation/src/connector/fetcher/sderFetcher.ts
@@ -1,29 +1,9 @@
 import { sderApi } from '../../sderApi';
-import { Deprecated } from '@label/core';
 
 export { sderFetcher };
 
 const sderFetcher = {
-  async fetchCourtDecisionBySourceIdAndSourceName(
-    sourceId: number,
-    sourceName: Deprecated.Sources,
-  ) {
-    return sderApi.fetchCourtDecisionBySourceIdAndSourceName(
-      sourceId,
-      sourceName,
-    );
-  },
-
-  async fetchDecisionsToPseudonymise(sourceName: string) {
-    const courtDecisions = await sderApi.fetchDecisionsToPseudonymise(
-      sourceName,
-    );
-
-    return courtDecisions?.filter((courtDecision) => {
-      if (!courtDecision.originalText) {
-        return false;
-      }
-      return true;
-    });
-  },
+  fetchCourtDecisionBySourceIdAndSourceName:
+    sderApi.fetchCourtDecisionBySourceIdAndSourceName,
+  fetchDecisionsToPseudonymise: sderApi.fetchDecisionsToPseudonymise,
 };
